4e0554461d69e361a94f507262f3a1505ca60f68
hiluluke <hiluluke@hotmail.com>
2017-08-25 10:52:38 +0800

    add module

--- config/config.js
+++ config/config.js
@@ -11,17 +11,16 @@
 var config = {
 	port: 8080,
 	ipWhitelist: ["127.0.0.1", "::ffff:127.0.0.1", "::1"], // Set [] to allow all IP addresses
-	                                                       // or add a specific IPv4 of 192.168.1.5 :
-	                                                       // ["127.0.0.1", "::ffff:127.0.0.1", "::1", "::ffff:192.168.1.5"],
-	                                                       // or IPv4 range of 192.168.3.0 --> 192.168.3.15 use CIDR format :
-	                                                       // ["127.0.0.1", "::ffff:127.0.0.1", "::1", "::ffff:192.168.3.0/28"],
+	// or add a specific IPv4 of 192.168.1.5 :
+	// ["127.0.0.1", "::ffff:127.0.0.1", "::1", "::ffff:192.168.1.5"],
+	// or IPv4 range of 192.168.3.0 --> 192.168.3.15 use CIDR format :
+	// ["127.0.0.1", "::ffff:127.0.0.1", "::1", "::ffff:192.168.3.0/28"],
 
 	language: "en",
 	timeFormat: 24,
 	units: "metric",
 
-	modules: [
-		{
+	modules: [{
 			module: "alert",
 		},
 		{
@@ -37,12 +36,10 @@ var config = {
 			header: "CHINA Holidays",
 			position: "top_left",
 			config: {
-				calendars: [
-					{
-						symbol: "calendar-check-o",
-						url: "http://7xlcyv.com1.z0.glb.clouddn.com/China_Holidays.ics"
-					}
-				]
+				calendars: [{
+					symbol: "calendar-check-o",
+					url: "http://7xlcyv.com1.z0.glb.clouddn.com/China_Holidays.ics"
+				}]
 			}
 		},
 		{
@@ -70,14 +67,12 @@ var config = {
 			module: "MMM-MirrorMirrorOnTheWall",
 			position: "middle_center",
 			config: {}
-		},
-		{
+		}, {
 			module: "newsfeed",
 			position: "top_right",
 			header: "Hot technology News",
 			config: {
-				feeds: [
-					{
+				feeds: [{
 						title: "InfoQ",
 						url: "http://www.infoq.com/cn/feed"
 					},
@@ -90,19 +85,27 @@ var config = {
 				showSourceTitle: true,
 				showPublishDate: true
 			}
-		}, {
-			module: "currentweather",
-			position: "top_right",	// This can be any of the regions.
+		},
+		{
+			module: 'MMM-AirQuality',
+			position: 'top_right', // you may choose any location
 			config: {
-				// See 'Configuration options' for more information.
-				location: "Amsterdam,Netherlands",
-				locationID: "1790630", //Location ID from http://openweathermap.org/help/city_list.txt
-				appid: "abcde12345abcde12345abcde12345ab" //openweathermap.org API key.
+				location: 'xian' // the location to check the index for
 			}
-		}
+		}, {
+			module: 'MMM-bitcoin',
+			position: 'top_right',
+			config: {
+				fiat: 'usd', // 'usd' and 'eur' available, defaults to 'usd'
+				showBefore: 'Bitstamp', // will display before the bitcoin price, default 'Bitstamp'
+				updateInterval: 60000 // update interval in milliseconds
+			}
+		},
 	]
 
 };
 
 /*************** DO NOT EDIT THE LINE BELOW ***************/
-if (typeof module !== "undefined") {module.exports = config;}
+if (typeof module !== "undefined") {
+	module.exports = config;
+}
--- modules/default/newsfeed/newsfeed.js
+++ modules/default/newsfeed/newsfeed.js
@@ -99,7 +99,7 @@ Module.register("newsfeed",{
 			// this.config.showFullArticle is a run-time configuration, triggered by optional notifications
 			if (!this.config.showFullArticle && (this.config.showSourceTitle || this.config.showPublishDate)) {
 				var sourceAndTimestamp = document.createElement("div");
-				sourceAndTimestamp.className = "light small dimmed";
+				sourceAndTimestamp.className = "small dimmed";
 
 				if (this.config.showSourceTitle && this.newsItems[this.activeItem].sourceTitle !== "") {
 					sourceAndTimestamp.innerHTML = this.newsItems[this.activeItem].sourceTitle;
